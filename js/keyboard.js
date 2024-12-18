ca5a677a98004470d4221a476c6a060832a2094d
FionaSeahShuHui <fionaseah2606@gmail.com>
2024-12-18 20:11:53 +0800

    chore: convert  keyboard to upper case

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,8 +1,14 @@
 const keyboardDisplay = document.getElementById("keyboard");
 
-for (let i = 97; i <123; i++) {
+for (let i = 65; i <91; i++) {
     const keyboardButton = document.createElement("button");
     keyboardButton.classList.add("keyboardButton");
     keyboardButton.innerText = String.fromCharCode(i);
     keyboardDisplay.appendChild(keyboardButton);
+
+    keyboardButton.addEventListener("click", function () {
+        const letterGuess = keyboardButton.innerText.toLowerCase()
+        console.log(letterGuess);
+        handleGuess(letterGuess);
+    });
 }
