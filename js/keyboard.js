3ef28e31284010881b9ac1ebed1978740fd838d2
FionaSeahShuHui <fionaseah2606@gmail.com>
2024-12-18 22:10:32 +0800

    chore: remove console log lines

--- js/keyboard.js
+++ js/keyboard.js
@@ -12,14 +12,11 @@ for (let i = 65; i <91; i++) {
 
         keyboardButton.disabled = true;
         keyboardButton.classList.add("disabled");
-
-        console.log(letterGuess);
     });
 }
 
 function initializeKeyboard() {
     const allButtons = document.querySelectorAll(".keyboardButton");
-    
     allButtons.forEach(button => {
         button.disabled = false;  // Enable the button
         button.classList.remove("disabled");  // Remove the disabled styling
--- js/wordGuessGenerator.js
+++ js/wordGuessGenerator.js
@@ -9,16 +9,16 @@ let word = "";
 initializeGame();
 
 function initializeGame() {
+    initializeKeyboard();
     wordGuess.innerHTML = "";
     wrongGuessCount = 0;
     randomWord = getRandomGameWord();
-    console.log("New Word: "+randomWord);
     for (let i = 0; i < randomWord.length; i++) {
         const letterBox = document.createElement("div");
         letterBox.className = "wordGuessLetter";
         wordGuess.appendChild(letterBox);
     }
-    initializeKeyboard();
+    
     hangmanImage.src = "./assets/images/hangman/0.png";
 }
 
@@ -33,13 +33,12 @@ function handleGuess(letter) {
     }
 
     if (!isCorrect) {
-        console.log("Wrong guess!");
         wrongGuessCount += 1
         hangmanImage.src = `./assets/images/hangman/${wrongGuessCount}.png`;
     }
 
+    console.log(wrongGuessCount)
     if (wrongGuessCount >= maxWrongGuessCount) {
-        console.log("Game Over")
         alert("Game Over. The word is "+randomWord)
         initializeGame()
     }
